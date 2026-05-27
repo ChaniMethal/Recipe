99afeb0e3e51fed654c1469c96f007082badac31
ChaniMethal <chani.silberberg@gmail.com>
2026-05-27 05:16:55 -0400

    update

--- Sprocs/Recipe Update.sql
+++ Sprocs/Recipe Update.sql
@@ -1,17 +1,45 @@
-create or alter proc RecipeUpdate(
+create or alter proc dbo.RecipeUpdate(
     @RecipeId int output,
     @CuisineId int,
     @UserId int,
-    @RecipeName varchar (100),
+    @RecipeName varchar(100),
     @Calories int,
+    @DateCreated datetime,
+    @Message varchar(500) = '' output
 )
 as 
 begin 
     declare @return int = 0
 
-    select @RecipeId = nullif(@RecipeId,0), @TermEnd = nullif(@TermEnd,0), @Num = ISNULL(@Num,0)
-    if @TermEnd is null and exists (select * from President p where p.TermEnd is null and p.PresidentId <> @PresidentId)
-    begin 
-        select @return = 1, @Message = 'There can be only one current president at a time.'
+    select 
+        @RecipeId = nullif(@RecipeId, 0),
+        @Calories = isnull(@Calories, 0)
+
+    if @Calories <= 0
+    begin
+        select @return = 1, @Message = 'Calories must be greater than zero.'
         goto finished
     end
+
+    if @RecipeId is null
+    begin
+        insert Recipe(CuisineId, UserId, RecipeName, Calories, DateCreated)
+        values(@CuisineId, @UserId, @RecipeName, @Calories, @DateCreated)
+
+        select @RecipeId = scope_identity()
+    end
+    else
+    begin
+        update Recipe
+        set 
+            CuisineId = @CuisineId,
+            UserId = @UserId,
+            RecipeName = @RecipeName,
+            Calories = @Calories
+        where RecipeId = @RecipeId
+    end
+
+    finished:
+    return @return
+end
+go
