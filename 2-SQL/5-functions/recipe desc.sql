c3fe203f8f71105a0a69247b3f385a10bcf3f804
ChaniMethal <chani.silberberg@gmail.com>
2026-06-02 04:37:42 -0400

    functions

--- 2-SQL/5-functions/recipe desc.sql
+++ 2-SQL/5-functions/recipe desc.sql
@@ -4,19 +4,21 @@ as
 begin 
     declare @value varchar(250) = ''
 
-    select @value = CONCAT(r.RecipeName, ' (', c.CuisineName, ') has ', count(i.IngredientId), ' ingredients and ', count(p.PrepStepsId), ' steps.' )
-    from recipe r 
+    select @value = concat(r.RecipeName, ' (', c.CuisineName, ') has ', count(distinct i.IngredientId),' ingredients and ',
+        count(distinct p.PrepStepsId),' steps.')
+    from Recipe r 
     left join Cuisine c 
-    on c.CuisineId = r.CuisineId
+        on c.CuisineId = r.CuisineId
     left join IngredientDesc i 
-    on i.RecipeId = r.RecipeId 
+        on i.RecipeId = r.RecipeId 
     left join PrepSteps p
-    on p.RecipeId = r.RecipeId
+        on p.RecipeId = r.RecipeId
     where r.RecipeId = @RecipeId
-    group by r.RecipeName, c.CuisineName, i.IngredientId, p.PrepStepsId
+    group by r.RecipeId, r.RecipeName, c.CuisineName
 
     return @value
 end 
 go
-select RecipeDesc = dbo.RecipeDesc(r.RecipeId),*
-from Recipe r 
+
+select RecipeDesc = dbo.RecipeDesc(r.RecipeId), *
+from Recipe r
