d8d550a0bc3e4ab918869066f9f5e04379bc4b38
ChaniMethal <chani.silberberg@gmail.com>
2026-07-05 08:17:52 -0400

    connection string

--- 1-VisualStudio/RecipeApps/RecipeWinForms/Program.cs
+++ 1-VisualStudio/RecipeApps/RecipeWinForms/Program.cs
@@ -13,8 +13,8 @@ namespace RecipeWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //DBManager.SetConnectionString("Server = tcp:chanim.database.windows.net,1433; Initial Catalog = RecipeDB; Persist Security Info = False; User ID = ChaniM; Password = Azure2026!; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
-            DBManager.SetConnectionString("Server=.\\SQLExpress;Database=RecipeDB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;");
+            DBManager.SetConnectionString("Server = tcp:chanim.database.windows.net,1433; Initial Catalog = RecipeDB; Persist Security Info = False; User ID = ChaniM; Password = Azure2026!; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            
             Application.Run(new frmDashboard());
         }
     }
